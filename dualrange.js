3302fa99dd94b1c6762e30f484cd1e192f798764
Filipe Guarnieri <filipe@ibaro.com.br>
2018-02-23 15:26:52 -0300

    [ADD] Custom Style;[FIX] Steps Attributes

--- dualrange.js
+++ dualrange.js
@@ -1,42 +1,51 @@
 class DualRange {
-  constructor(element, fallback){
+  constructor(element, callback){
     document.querySelectorAll(element).forEach(item => {
+      const _parent = item.parentNode,
+      _inputClass = item.className;
+      item.className="";
       item.classList.add('dualrange__input-max');
+      if(!item.max){item.max=100;}
+      if(!item.min){item.min=0;}
       item.setAttribute('value', item.max||100);
       item.setAttribute('step', item.step||0.1);
       item.setAttribute('data-dualrange-max', '');
-      item.outerHTML = `<div class="dualrange" data-dualrange-valmin="${item.min}" data-dualrange-valmax="${item.max}"><input data-dualrange-min step="0.1" type="range" min="${item.min}" max="${item.max}" value="${item.min}" class="dualrange__input-min">${item.outerHTML}<div class="dualrange__min" style="left: 0%; transform: translate(0%, -50%);"></div><div class="dualrange__max" style="left: 100%; transform: translate(-100%, -50%);"></div><div class="dualrange__range"></div></div>`;
-    });
-    this.init(element);
-    this.fn = fallback;
-  }
-  init(element){
-    document.querySelectorAll('.dualrange input').forEach(item => {
-      this.range(item);
+      item.outerHTML = `<div class="dualrange ${_inputClass}" data-dualrange-valmin="${item.min}" data-dualrange-valmax="${item.max}"><input data-dualrange-min step="0.1" type="range" min="${item.min}" max="${item.max}" value="${item.min}" class="dualrange__input-min">${item.outerHTML}<div class="dualrange__min" style="left: 0%; transform: translate(0%, -50%);"></div><div class="dualrange__max" style="left: 100%; transform: translate(-100%, -50%);"></div><div class="dualrange__range"></div></div>`;
+      this.init(_parent, callback);
     });
   }
-  range(tag){
+  init(element, callback){
+    element.querySelectorAll('.dualrange input').forEach((item, i) => {
+      item.step = (i===0) ? item.nextElementSibling.step : item.step;
+      this.range(item, callback);
+    });
+  }
+  range(tag, callback){
     const _parent = tag.parentNode,
     _child = _parent.children;
     tag.addEventListener('input', event => {
       const _left = 100 / (tag.max - tag.min) * (tag.value-tag.min),
       _min = parseFloat(_child[0].value),
       _max = parseFloat(_child[1].value),
+      _steps = parseFloat(_child[0].step),
       _event = new Event('input');
       if(_min > _max){
-        _child[0].value = _min-0.1;
-        _child[1].value = _max+0.1;
+        _child[0].value = _min - _steps;
+        _child[1].value = _max + _steps;
         _child[0].dispatchEvent(_event);
         _child[1].dispatchEvent(_event);
         return false;
       }
       _child[tag.hasAttribute('data-dualrange-max') ? 3 : 2].style.left = `${_left}%`;
       _child[tag.hasAttribute('data-dualrange-max') ? 3 : 2].style.transform = `translate(-${_left}%, -50%)`;
-      if(this.fn){
-        clearTimeout(window.dualRangeFallback);
-        window.dualRangeFallback = setTimeout(() => {
-          this.fallback(tag, this.fn);
-        }, 100);
+      if(callback){
+        clearTimeout(window.dualRangeCallback);
+        window.dualRangeCallback = setTimeout(() => {
+          callback({
+            min: parseFloat(tag.parentNode.children[0].value),
+            max: parseFloat(tag.parentNode.children[1].value)
+          });
+        }, 10);
       }
     });
 
@@ -52,10 +61,7 @@ class DualRange {
       _parent.querySelector(_minRange ? '.dualrange__input-max' : '.dualrange__input-min').classList.remove('dualrange__zindex');
     });
   }
-  fallback(input, fn){
-    fn({
-      min: parseFloat(input.parentNode.children[0].value),
-      max: parseFloat(input.parentNode.children[1].value)
-    });
+  callback(input, fn){
+    fn();
   }
 }
